bae9d66609a630e281efdd8ce40f8c39396c77df
Marcin Sławicz <marcin.slawicz@gmail.com>
2022-05-27 22:27:19 +0200

    SVM look up table files added

---
+++ Core/Inc/SVM_lut.h
@@ -0,0 +1,15 @@
+/*
+ * SVM_lut.h
+ *
+ *  Created on: 27 maj 2022
+ *      Author: marci
+ */
+
+#ifndef INC_SVM_LUT_H_
+#define INC_SVM_LUT_H_
+
+#include "stm32f4xx_hal.h"
+
+int16_t getSVMvalue(int32_t angle);
+
+#endif /* INC_SVM_LUT_H_ */
