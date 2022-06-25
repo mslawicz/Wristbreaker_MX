09b57ad3f45e4dd42d911813d143a4cac5d5b801
Marcin Sławicz <marcin.slawicz@gmail.com>
2022-06-25 14:54:41 +0200

    USB HID descriptor comments corrected

--- USB_DEVICE/App/usbd_custom_hid_if.c
+++ USB_DEVICE/App/usbd_custom_hid_if.c
@@ -91,70 +91,70 @@
 __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END =
 {
   /* USER CODE BEGIN 0 */
-    // joystick report
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x04,                    // USAGE (Joystick)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x85, 0x01,                    //   REPORT_ID (1)
-    0x05, 0x01,                    //   USAGE_PAGE (Generic Desktop)
-    0x09, 0x01,                    //   USAGE (Pointer)
-    0xa1, 0x00,                    //   COLLECTION (Physical)
-    0x75, 0x10,                    //     REPORT_SIZE (16)
-    0x16, 0x01, 0x80,              //     LOGICAL_MINIMUM (-32767)
-    0x26, 0xff, 0x7f,              //     LOGICAL_MAXIMUM (32767)
-    0x09, 0x30,                    //     USAGE (X)
-    0x09, 0x31,                    //     USAGE (Y)
-    0x09, 0x32,                    //     USAGE (Z)
-    0x09, 0x35,                    //     USAGE (Rz)
-    0x95, 0x04,                    //     REPORT_COUNT (4)
-    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
-    0x26, 0xff, 0x7f,              //     LOGICAL_MAXIMUM (32767)
-    0x09, 0x33,                    //     USAGE (Rx)
-    0x09, 0x34,                    //     USAGE (Ry)
-    0x09, 0x36,                    //     USAGE (slider)
-    0x09, 0x37,                    //     USAGE (dial)
-    0x95, 0x04,                    //     REPORT_COUNT (4)
-    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-    0xc0,                          //   END_COLLECTION
-    0x09, 0x39,                    //   USAGE (Hat switch)
-    0x15, 0x01,                    //   LOGICAL_MINIMUM (1)
-    0x25, 0x08,                    //   LOGICAL_MAXIMUM (8)
-    0x35, 0x00,                    //   PHYSICAL_MINIMUM (0)
-    0x46, 0x3b, 0x01,              //   PHYSICAL_MAXIMUM (315)
-    0x65, 0x14,                    //   UNIT (Eng Rot:Angular Pos)
-    0x75, 0x04,                    //   REPORT_SIZE (4)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x81, 0x42,                    //   INPUT (Data,Var,Abs, Null)
-    0x75, 0x04,                    //   REPORT_SIZE (4)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x81, 0x41,                    //   INPUT (Cnst,Ary,Abs,Null)
-    0x05, 0x09,                    //   USAGE_PAGE (Button)
-    0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
-    0x29, 0x20,                    //   USAGE_MAXIMUM (Button 32)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    //   REPORT_SIZE (1)
-    0x95, 0x20,                    //   REPORT_COUNT (32)
-    0x55, 0x00,                    //   UNIT_EXPONENT (0)
-    0x65, 0x00,                    //   UNIT (None)
-    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-    0xc0,                          // END_COLLECTION
+    /* joystick report */
+    0x05, 0x01,                    /* USAGE_PAGE (Generic Desktop) */
+    0x09, 0x04,                    /* USAGE (Joystick) */
+    0xa1, 0x01,                    /* COLLECTION (Application) */
+    0x85, 0x01,                    /*   REPORT_ID (1) */
+    0x05, 0x01,                    /*   USAGE_PAGE (Generic Desktop) */
+    0x09, 0x01,                    /*   USAGE (Pointer) */
+    0xa1, 0x00,                    /*   COLLECTION (Physical) */
+    0x75, 0x10,                    /*     REPORT_SIZE (16) */
+    0x16, 0x01, 0x80,              /*     LOGICAL_MINIMUM (-32767) */
+    0x26, 0xff, 0x7f,              /*     LOGICAL_MAXIMUM (32767) */
+    0x09, 0x30,                    /*     USAGE (X) */
+    0x09, 0x31,                    /*     USAGE (Y) */
+    0x09, 0x32,                    /*     USAGE (Z) */
+    0x09, 0x35,                    /*     USAGE (Rz) */
+    0x95, 0x04,                    /*     REPORT_COUNT (4) */
+    0x81, 0x02,                    /*     INPUT (Data,Var,Abs) */
+    0x15, 0x00,                    /*     LOGICAL_MINIMUM (0) */
+    0x26, 0xff, 0x7f,              /*     LOGICAL_MAXIMUM (32767) */
+    0x09, 0x33,                    /*     USAGE (Rx) */
+    0x09, 0x34,                    /*     USAGE (Ry) */
+    0x09, 0x36,                    /*     USAGE (slider) */
+    0x09, 0x37,                    /*     USAGE (dial) */
+    0x95, 0x04,                    /*     REPORT_COUNT (4) */
+    0x81, 0x02,                    /*     INPUT (Data,Var,Abs) */
+    0xc0,                          /*   END_COLLECTION */
+    0x09, 0x39,                    /*   USAGE (Hat switch) */
+    0x15, 0x01,                    /*   LOGICAL_MINIMUM (1) */
+    0x25, 0x08,                    /*   LOGICAL_MAXIMUM (8) */
+    0x35, 0x00,                    /*   PHYSICAL_MINIMUM (0) */
+    0x46, 0x3b, 0x01,              /*   PHYSICAL_MAXIMUM (315) */
+    0x65, 0x14,                    /*   UNIT (Eng Rot:Angular Pos) */
+    0x75, 0x04,                    /*   REPORT_SIZE (4) */
+    0x95, 0x01,                    /*   REPORT_COUNT (1) */
+    0x81, 0x42,                    /*   INPUT (Data,Var,Abs, Null) */
+    0x75, 0x04,                    /*   REPORT_SIZE (4) */
+    0x95, 0x01,                    /*   REPORT_COUNT (1) */
+    0x81, 0x41,                    /*   INPUT (Cnst,Ary,Abs,Null) */
+    0x05, 0x09,                    /*   USAGE_PAGE (Button) */
+    0x19, 0x01,                    /*   USAGE_MINIMUM (Button 1) */
+    0x29, 0x20,                    /*   USAGE_MAXIMUM (Button 32) */
+    0x15, 0x00,                    /*   LOGICAL_MINIMUM (0) */
+    0x25, 0x01,                    /*   LOGICAL_MAXIMUM (1) */
+    0x75, 0x01,                    /*   REPORT_SIZE (1) */
+    0x95, 0x20,                    /*   REPORT_COUNT (32) */
+    0x55, 0x00,                    /*   UNIT_EXPONENT (0) */
+    0x65, 0x00,                    /*   UNIT (None) */
+    0x81, 0x02,                    /*   INPUT (Data,Var,Abs) */
+    0xc0,                          /* END_COLLECTION */
 
-    // define generic data buffer for simulator data reception
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x00,                    // USAGE (Undefined)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x85, 0x02,                    //   REPORT_ID (2)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x95, 0x3F,                    //   REPORT_COUNT (63)
-    0x09, 0x00,                    //   USAGE (Undefined)
-    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-    0x95, 0x3F,                    //   REPORT_COUNT (63)
-    0x09, 0x00,                    //   USAGE (Undefined)
-    0x91, 0x02,                    //   OUTPUT (Data,Var,Abs)
+    /* define generic data buffer for simulator data reception */
+    0x05, 0x01,                    /* USAGE_PAGE (Generic Desktop) */
+    0x09, 0x00,                    /* USAGE (Undefined) */
+    0xa1, 0x01,                    /* COLLECTION (Application) */
+    0x85, 0x02,                    /*   REPORT_ID (2) */
+    0x15, 0x00,                    /*   LOGICAL_MINIMUM (0) */
+    0x26, 0xff, 0x00,              /*   LOGICAL_MAXIMUM (255) */
+    0x75, 0x08,                    /*   REPORT_SIZE (8) */
+    0x95, 0x3F,                    /*   REPORT_COUNT (63) */
+    0x09, 0x00,                    /*   USAGE (Undefined) */
+    0x81, 0x02,                    /*   INPUT (Data,Var,Abs) */
+    0x95, 0x3F,                    /*   REPORT_COUNT (63) */
+    0x09, 0x00,                    /*   USAGE (Undefined) */
+    0x91, 0x02,                    /*   OUTPUT (Data,Var,Abs) */
   /* USER CODE END 0 */
   0xC0    /*     END_COLLECTION	             */
 };
